9545011bc711a11f2934d161b3dd24a7921037db
Mathis Leclair <mleclair@student.42.fr>
2016-12-12 13:56:11 +0100

    OK

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/13 13:37:01 by mleclair          #+#    #+#             */
+/*   Updated: 2016/11/14 15:09:03 by mleclair         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "./libft/libft.h"
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
